f4237f45adb6bdff1af7e54c4fa0adbbeb04b8d5
AxesPL <pawel.kaszuba@gazeta.pl>
2017-10-24 20:52:20 +0200

    Poprawienie blednych folderow

--- src/main/java/cepik/CepikApplication.java
+++ src/main/java/cepik/CepikApplication.java
@@ -1,4 +1,12 @@
 package cepik;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CepikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepikApplication.class, args);
+	}
 }
