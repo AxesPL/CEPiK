f3a9fa0d77dee772f8ef90238b811b9a228f3ef9
AxesPL <pawel.kaszuba@gazeta.pl>
2017-10-26 00:01:10 +0200

    Dodano konfigurację połączenia z bazą danych CEPIK, dodana możliwość tworzenia encji Pojazd, przeniesiono plik .gitignre z develop

--- src/main/java/cepik/model/Pojazd.java
+++ src/main/java/cepik/model/Pojazd.java
@@ -2,8 +2,14 @@ package cepik.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
+@Entity
 public class Pojazd {
+    @Id
+    private Long id;
     private String marka;
     private String model;
 }
