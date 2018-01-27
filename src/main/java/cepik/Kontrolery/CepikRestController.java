4371359349f116e5955b1fdb62614c11c4bca554
Michal Karwowski <michal.karwowski.0@gmail.com>
2018-01-27 14:31:44 +0100

    Dodaje kontroler REST i serwis do wlaściciela

--- src/main/java/cepik/Kontrolery/CepikRestController.java
+++ src/main/java/cepik/Kontrolery/CepikRestController.java
@@ -3,22 +3,27 @@ package cepik.Kontrolery;
 import cepik.Enumy.Wynik;
 import cepik.Model.Pojazd;
 import cepik.Serwisy.ObslugaPojazdService;
+import cepik.Serwisy.ObslugaWlascicielService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("/cepik")
 public class CepikRestController {
 
     @Autowired
     private ObslugaPojazdService obslugaPojazdService;
+    @Autowired
+    private ObslugaWlascicielService obslugaWlascicielService;
 
     @RequestMapping(path = "/pojazd",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
     public Wynik zapiszDaneOPojezdzie(@RequestBody Pojazd pojazd){
         return obslugaPojazdService.dodajPojazd(pojazd);
+    }
+
+    @RequestMapping(path = "/wlasciciel", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Wynik zapiszDaneOWlascicielu(@RequestBody cepik.Model.Wlasciciel wlasciciel) {
+        return obslugaWlascicielService.dodajWlasciciela(wlasciciel);
     }
 
 }
---
+++ src/main/java/cepik/Serwisy/ObslugaWlascicielService.java
@@ -0,0 +1,27 @@
+package cepik.Serwisy;
+
+import cepik.Enumy.Wynik;
+import cepik.Model.Wlasciciel;
+import cepik.Repository.WlascicielRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+
+@Service
+public class ObslugaWlascicielService {
+
+    @Autowired
+    private WlascicielRepository wlascicielRepository;
+
+    @Transactional
+    public Wynik dodajWlasciciela(Wlasciciel wlasciciel){
+        try {
+            wlascicielRepository.save(wlasciciel);
+            return Wynik.SUKCES;
+        } catch (Exception e) {
+            return Wynik.BLAD;
+        }
+    }
+}
